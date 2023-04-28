491b5f3c5991320d6ff7b8acd6b93a4b05cf8599
unknown <js4real@naver.com>
2023-04-29 02:45:30 +0900

    scr폴더로 분류

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
 "use strict";
 
-// 모듈
+//모듈
 const express = require("express");
 const app = express();
 
+//라우팅
+const home = require("../routes/home");
 
-// 라우팅
-const home = require("./routes/home");
-
-
-// 앱 세팅
+//앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-
-app.use("/", home); // use -> 미들 웨어 등록 메서드
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 
 module.exports = app;
