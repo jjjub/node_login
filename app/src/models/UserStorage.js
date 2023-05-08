902a2d7f929f226a0f7bda8090eea727893565cd
unknown <js4real@naver.com>
2023-05-09 00:11:53 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,10 @@
 "use strict"
 
-class UserStorage{
-    static #users = {  // # : 은닉화
-        id : ["js4real", "윤준섭", "윤준"],
-        pwd : ["1234", "64742713", "1234"],
-        name : ["윤준섭", "윤", "준섭"],
-    };
+const fs = require("fs").promises;
 
+class UserStorage{
     static getUsers (...fields) {
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
@@ -19,19 +15,26 @@ class UserStorage{
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);  // => [id, pwd, name]
-        const userInfo = usersKeys.reduce((newUser, info)=>{
-            newUser[info] = users[info][idx];
-            return newUser;
+        return fs
+        .readFile("./src/DB/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);         
+    }
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+            const idx = users.id.indexOf(id);
+            const usersKeys = Object.keys(users);  // => [id, pwd, name]
+            const userInfo = usersKeys.reduce((newUser, info)=> {
+                newUser[info] = users[info][idx];
+                return newUser;
         }, {});
-
-        return userInfo
+        return userInfo;
     }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.pwd.push(userInfo.pwd);
